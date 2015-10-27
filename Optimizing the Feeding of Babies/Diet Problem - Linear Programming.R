d96aab9d74de2e446bff00f6c2249d46e87de8f3
JJ Espinoza <espin086@gmail.com>
2015-10-26 22:05:15 -0700

    deleted extra blank rows and import data
    deleted extra rows in data, created R code to import data from Github

--- Optimizing the Feeding of Babies/Diet Problem - Linear Programming.R
+++ Optimizing the Feeding of Babies/Diet Problem - Linear Programming.R
@@ -1,2 +1,13 @@
 ########################################
 #Feeding Babies
+
+#Importing data from Github
+
+library(RCurl)
+library(foreign)
+
+
+url <- "https://raw.githubusercontent.com/espin086/data-dad/master/Optimizing%20the%20Feeding%20of%20Babies/Baby%20Food.csv"
+baby.food <- getURL(url)                
+baby.food <- read.csv(textConnection(baby.food), header = TRUE, skip =1)
+
