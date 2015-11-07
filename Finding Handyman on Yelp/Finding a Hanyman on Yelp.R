eb39af966a31f560b54050734ead497a2ce499ae
JJ Espinoza <espin086@gmail.com>
2015-11-07 15:35:06 -0800

    listing calls and uncalled

--- Finding Handyman on Yelp/Finding a Hanyman on Yelp.R
+++ Finding Handyman on Yelp/Finding a Hanyman on Yelp.R
@@ -9,9 +9,24 @@ url <- "https://docs.google.com/spreadsheets/d/1kkBgMg3Q5-Nmm-lgNmDSZpwIK520g1gg
 handy <- getURL(url)                
 handy <- read.csv(textConnection(handy), header = TRUE)
 
+#Based on Calls eliminae these people from further analysis.
+handy <- cbind(handy, notes = "Uncalled")
+handy$notes <- as.character(handy$notes)
+
+handy$notes[handy$Name == "Your Local Handyman, Inc" | 
+              handy$Name =="Webbâs Handyman Service" | 
+              handy$Name =="Victor White Handyman" | handy$Name =="Professional Handyman" ] <- "Messaged"
+
+handy$notes[handy$Name == "M.Y. Handyman Services" | handy$Name == "Alâs Handyman Service"] <- "No SFS"
+
+handy.list <- split(handy, handy$notes)
+
+uncalled <- handy.list$Uncalled
+
+
 #Summary of the data
-str(handy)
-summary(handy)
+str(uncalled)
+summary(uncalled)
 
 #Set Seed
 set.seed(2015)
@@ -22,7 +37,7 @@ list.within <- NULL
 
 for (i in 1:10) {
   #Clustering Exercise
-  km.out = kmeans(scale(handy[2:3]), i, nstart = 100)
+  km.out = kmeans(scale(uncalled[2:3]), i, nstart = 100)
   
   #Evaluating the clusters based on total within sum of squares
   list.within <- rbind(list.within,km.out$tot.withinss)
@@ -34,21 +49,21 @@ plot(list.within,type="b", main = "Sum of Squared Scree Plot" , xlab="Number of 
      ylab="Within groups sum of squares", col = "red")
 
 #Performig the optimal clustering based on the plot above.
-km.out = kmeans(scale(handy[2:3]), 6, nstart = 100)
+km.out = kmeans(scale(uncalled[2:3]), 6, nstart = 100)
 
 #Summarizing clusters and visualizing in two dimentional graph
 km.out
-plot(handy[2:3], col = (km.out$cluster + 1), main = "K-Means Clustering of Handymen", ylab="Yelp Stars", xlab="Yelp Reviews", pch=20, cex=2)
+plot(uncalled[2:3], col = (km.out$cluster + 1), main = "K-Means Clustering of Handymen", ylab="Yelp Stars", xlab="Yelp Reviews", pch=20, cex=2)
 #Appending to original data
-handy.clustered <- cbind(handy, km.out$cluster)
+uncalled.clustered <- cbind(uncalled, km.out$cluster)
 
 
 ###############################################
 #Hierarchical Clustering
 
-hc.complete <- hclust(dist(scale(handy[2:3])), method="complete")
-hc.average <- hclust(dist(scale(handy[2:3])), method="average")
-hc.single <- hclust(dist(scale(handy[2:3])), method="single")
+hc.complete <- hclust(dist(scale(uncalled[2:3])), method="complete")
+hc.average <- hclust(dist(scale(uncalled[2:3])), method="average")
+hc.single <- hclust(dist(scale(uncalled[2:3])), method="single")
 
 #Visualizing the clusters
 par(mfrow=c(1,3))
@@ -57,21 +72,23 @@ plot(hc.average, main = "Average Linkage", xlab = "", ylab="")
 plot(hc.single, main = "Single Linkage", xlab = "", ylab="")
 
 
-handy.clustered <- cbind(handy.clustered, cutree(hc.complete, 6))
-handy.clustered <- cbind(handy.clustered, cutree(hc.average, 6))
-handy.clustered <- cbind(handy.clustered, cutree(hc.single, 6))
+uncalled.clustered <- cbind(uncalled.clustered, cutree(hc.complete, 6))
+uncalled.clustered <- cbind(uncalled.clustered, cutree(hc.average, 6))
+uncalled.clustered <- cbind(uncalled.clustered, cutree(hc.single, 6))
 
 
 ###############################################
 #Visualizing Clustering Techniques shows that 3 of these reviews stand out in terms of 
 #high reviews and high ratings
 par(mfrow=c(1,4))
-plot(handy.clustered[2:3], col = (km.out$cluster + 1), main = "K-Means Clustering", ylab="Yelp Stars", xlab="Yelp Reviews", pch=20, cex=2)
-plot(handy.clustered[2:3], col = (handy.clustered$`cutree(hc.complete, 6)` + 1), main = "Hierarchical Clustering - Complete", ylab="Yelp Stars", xlab="Yelp Reviews", pch=20, cex=2)
-plot(handy.clustered[2:3], col = (handy.clustered$`cutree(hc.average, 6)` + 1), main = "Hierarchical Clustering - Average", ylab="Yelp Stars", xlab="Yelp Reviews", pch=20, cex=2)
-plot(handy.clustered[2:3], col = (handy.clustered$`cutree(hc.single, 6)` + 1), main = "Hierarchical Clustering - Single", ylab="Yelp Stars", xlab="Yelp Reviews", pch=20, cex=2)
+plot(uncalled.clustered[2:3], col = (km.out$cluster + 1), main = "K-Means Clustering", ylab="Yelp Stars", xlab="Yelp Reviews", pch=20, cex=2)
+plot(uncalled.clustered[2:3], col = (uncalled.clustered$`cutree(hc.complete, 6)` + 1), main = "Hierarchical Clustering - Complete", ylab="Yelp Stars", xlab="Yelp Reviews", pch=20, cex=2)
+plot(uncalled.clustered[2:3], col = (uncalled.clustered$`cutree(hc.average, 6)` + 1), main = "Hierarchical Clustering - Average", ylab="Yelp Stars", xlab="Yelp Reviews", pch=20, cex=2)
+plot(uncalled.clustered[2:3], col = (uncalled.clustered$`cutree(hc.single, 6)` + 1), main = "Hierarchical Clustering - Single", ylab="Yelp Stars", xlab="Yelp Reviews", pch=20, cex=2)
+
 
 
 ##################
-#Selecting Handymen to call
-selected <- handy.clustered[which(handy.clustered$`km.out$cluster`==2 & handy.clustered$Stars > 4),]
+#Adding questions to ask Handymen
+uncalled <- cbind(uncalled, SFS ="",  licensed="", provide.quote.ahead="", work.guaranteed="", work.in.team="")
+
